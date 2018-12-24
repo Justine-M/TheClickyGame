cfcbd36f7262d1a4fadc2a3b25134dff79f7a358
Justine-M <justalexmays@hotmail.com>
2018-12-24 18:05:01 -0500

    updated images and read me

--- src/components/FriendCard/FriendCard.js
+++ src/components/FriendCard/FriendCard.js
@@ -10,21 +10,8 @@ const FriendCard = props => (
     <div className="img-container">
       <img alt={props.name} src={props.image} />
     </div>
-    <div className="content">
-      <ul>
-        <li>
-          <strong>Name:</strong> {props.name}
-        </li>
-        <li>
-          <strong>Occupation:</strong> {props.occupation}
-        </li>
-        <li>
-          <strong>Location:</strong> {props.location}
-        </li>
-      </ul>
-    </div>
   
-  </div>
+    </div>
 );
 
 export default FriendCard;
--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Title.css";
 
-const Title = props => <h1 className="title">{props.children}</h1>;
+const Title = props => <h6 className="title">{props.children}</h6>;
 
 
 export default Title;
